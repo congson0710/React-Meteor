2d8275efdf5b455b83ac7da9da95dc2ffcc8efb9
congson0710 <congson0710@gmail.com>
2018-06-04 21:31:11 +0700

    use axios to make AJAX request

--- client/main.js
+++ client/main.js
@@ -1,14 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import axios from 'axios'
 import ImageList from './components/ImageList'
 
-const App = () => (
-    <div>
-        <ImageList/>
-    </div>
-)
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            images: []
+        }
+    }
+
+    componentWillMount() {
+        axios({
+            method: 'get',
+            url: 'https://api.imgur.com/3/gallery/hot/viral/0',
+            headers: {
+                'authorization': 'Client-ID ' + '29052a0c342e6be'
+            }
+        }).then(response => this.setState({
+            images: response.data.data
+        }))
+    }
+
+    render() {
+        return ( 
+            <div >
+                <ImageList {...this.state} />
+            </div>
+        )
+    }
+}
 
 Meteor.startup(() => {
-    ReactDOM.render(<App/>, document.querySelector('.container'))
+    ReactDOM.render( < App /> , document.querySelector('.container'))
 })
