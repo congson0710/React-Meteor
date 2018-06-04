17dc9cd5a2c1288ef748017f86afb9aeda4402db
congson0710 <congson0710@gmail.com>
2018-06-04 20:22:49 +0700

    styling Image Detail component

---
+++ client/components/ImageDetail.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ImageDetail = () => {
+    return (
+        <li className="media list-group-item">
+            <div className="media-left">
+                Image
+            </div>
+            <div className="media-body">
+                <h4 className="media-heading">Title</h4>
+            </div>
+        </li>
+    )
+}
+
+export default ImageDetail
